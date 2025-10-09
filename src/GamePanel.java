7c37033044ece7a01bd85708e02545ad73c4e21a
nastea19 <nastea.popova1907@gmail.com>
2025-10-09 20:25:35 +0200

    created the thread in GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,11 +3,13 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class GamePanel extends JPanel {
-    
+//Runnable is the key for using Threads
+public class GamePanel extends JPanel implements Runnable {
+
     public GamePanel() {
         setBackground(Color.BLACK);
     }
+
     public static void main(String[] args) {
         // Dimensions and scaling of the screen
         int rowCount = 12;
@@ -24,14 +26,31 @@ public class GamePanel extends JPanel {
         frame.setSize(boardWidth, boardHeight); // sets the size of the frame
         frame.setLocationRelativeTo(null); // sets the window on the center of the screen
         frame.setResizable(false); // stops the user from resizing the window
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // determines if the user clicks on the x button of the window
 
-        
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+    }
+
+    // keeps the program running until you stop it
+    // useful for repetitive processes (fps)
+    Thread gameThread;
+
+    public void startGameThread() {
+        // initiation and starting of the game thread
+        // by "this" we are passing GamePanel class to this thread's constructor
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    // starting the thread causes the object's run method to be called in that
+    // separately executing thread
+    public void run() {
+        // throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 }

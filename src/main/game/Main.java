8e477a6670dfe61e4391ef8f68fd76676d417b21
kremenamkrumova-beep <kremena.m.krumova@gmail.com>
2025-10-16 13:21:14 +0300

    Main is not done

--- src/main/game/Main.java
+++ src/main/game/Main.java
@@ -6,14 +6,7 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Zombie Combat");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 420);
-
-        StartPanel startPanel = new StartPanel(frame);
-        frame.add(startPanel);
-
-        frame.setVisible(true);
+        
     }
     
 }

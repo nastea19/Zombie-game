a03612f39fded5d4250959694e1f95a374c4a8d0
kremenamkrumova-beep <kremena.m.krumova@gmail.com>
2025-10-16 13:17:10 +0300

    StartPanel added

--- src/main/game/Main.java
+++ src/main/game/Main.java
@@ -1,7 +1,19 @@
 package game;
 
+import javax.swing.JFrame;
+
 // import javax.swing.JFrame;
 
 public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Zombie Combat");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+
+        StartPanel startPanel = new StartPanel(frame);
+        frame.add(startPanel);
+
+        frame.setVisible(true);
+    }
     
 }
--- src/main/game/StartPanel.java
+++ src/main/game/StartPanel.java
@@ -1,5 +1,34 @@
 package game;
 
-public class StartPanel {
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class StartPanel extends JPanel{
+    private JFrame frame;
+
+    public StartPanel(JFrame frame) {
+        this.frame = frame;
+        setLayout(null);
+        setBounds(0, 0, 420, 420);;
+
+        JButton startButton = new JButton("Start Game");
+        startButton.setBounds(150, 180, 120, 40);
+        startButton.addActionListener((ActionEvent e) -> StartGame());
+        add(startButton);
+    }
+
+    private void StartGame() {
+        frame.getContentPane().removeAll();
+
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+
+        frame.revalidate();
+        frame.repaint();
+
+        gamePanel.startGameThread();
+    }
 }
